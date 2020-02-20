fed2c7af04108ccebacee7a47063fb324b3a0724
Knowblesse <knowblesse@gmail.com>
2020-02-20 11:25:59 +0900

    Motion detection function added

--- ArduinoCode/DoorController.ino
+++ ArduinoCode/DoorController.ino
@@ -1,6 +1,7 @@
 const int PIN_MOTOR_DIR_OUTPUT = 6;
 const int PIN_MOTOR_CLK_OUTPUT = 5;
 const int PIN_DOOR_STATE_INPUT = 4;
+const int PIN_MOTION_SENSOR_INPUT = 8;
 
 const int PIN_OPENBUTTON_INPUT = 10;
 const int PIN_CLOSESIG_INPUT = 9;
@@ -16,6 +17,14 @@ bool isDirectionUp;
 bool currPushButtonState;
 bool isPushButtonPressed;
 bool currControllerSignal;
+
+// variables for motion sensor
+bool isMotionDetected = false;
+bool currMotionSensorSignal;
+unsigned long motionOnSetTime;
+unsigned long motionCriteronTime = 1000;
+bool isMotionModeOn = false;
+bool isFirstTrial = true;
 
 bool isDoorMoving;
 unsigned long doorStartTime;
@@ -35,6 +44,8 @@ void setup()
   pinMode(PIN_OPENBUTTON_INPUT, INPUT);
   pinMode(PIN_CLOSESIG_INPUT, INPUT);
 
+  pinMode(PIN_MOTION_SENSOR_INPUT, INPUT);
+
   // Check toggle state
   prevToggleState = digitalRead(PIN_TOGGLE_INPUT);
 
@@ -49,6 +60,7 @@ void loop()
   currToggleState = digitalRead(PIN_TOGGLE_INPUT);
   currPushButtonState = digitalRead(PIN_OPENBUTTON_INPUT);
   currControllerSignal = digitalRead(PIN_CLOSESIG_INPUT);
+  currMotionSensorSignal = digitalRead(PIN_MOTION_SENSOR_INPUT) == HIGH;
   
   // set push button state for single button mode
   if (currPushButtonState == HIGH && isPushButtonPressed == false)
@@ -111,6 +123,7 @@ void loop()
       doorStartTime = millis();
       isDirectionUp = true;
       moveDoor(isDirectionUp);
+      isFirstTrial = false;
     }
     if (currControllerSignal == HIGH) // attack signal came. go down.
     {
@@ -119,17 +132,52 @@ void loop()
       isDirectionUp = false;
       moveDoor(isDirectionUp);  
     }
+
+    // motion sensor logic
+    if (isMotionModeOn && !isFirstTrial)
+    {
+      if (!isDirectionUp)//only works when the door is down position
+      {  
+         if (currMotionSensorSignal && !isMotionDetected)
+        {
+          isMotionDetected = true;
+          motionOnSetTime = millis();
+        }
+        else if (currMotionSensorSignal && isMotionDetected)
+        {
+          if (millis() - motionOnSetTime > motionCriteronTime)
+          {
+            doorStartTime = millis();
+            isDirectionUp = true;
+            moveDoor(isDirectionUp);
+            isMotionDetected = false;
+          }
+        }
+        else
+        {
+          isMotionDetected = false;
+        }
+      }
+    }
   }
 
   // Reset button
   if (digitalRead(PIN_RESET_INPUT))
   {
     noTone(PIN_MOTOR_CLK_OUTPUT); // stops everything
+    isFirstTrial = true;
     isDoorMoving = false;
     delay(1000);
+    if (digitalRead(PIN_RESET_INPUT)) // to enable the motion mode, press reset button for [1,2) sec
+    {
+      isMotionModeOn = true;
+      doorStartTime = millis();//to show the setting is complete, door comes down.
+      isDirectionUp = false;
+      moveDoor(isDirectionUp);  
+      delay(1000);
+    }
     prevToggleState = digitalRead(PIN_TOGGLE_INPUT); // prevent moving right after reset off
   }
-
 }
 
 void moveDoor(bool isDirectionUp)
