3bde41c1894c9cea2bb1998b98ada03b1a5852a7
Ji Hoon Jeong <knowblesse@gmail.com>
2022-08-24 10:34:13 +0900

    Minor change

--- MatlabCode/Spike/SortedPETH_Scripts.m
+++ MatlabCode/Spike/SortedPETH_Scripts.m
@@ -1,12 +1,12 @@
 %% SortedPETH_Scripts
-
-output = loadAllUnitData();
+load('C:\Users\Knowblesse\SynologyDrive\AllUnitData.mat')
+%output = loadAllUnitData();
 output_PL = output(output.Area == "PL", :);
 output_IL = output(output.Area == "IL", :);
 
 %% Responsiveness calculation
 
-unitData = output_IL;
+unitData = output;
 
 zscore_threshold = 4;
 bin_size = 80;
@@ -31,8 +31,6 @@ fprintf('LK Responsive  : %.2f %%\n', sum(responsive(:,1)) / size(unitData,1)  *
 fprintf('AHW Responsive : %.2f %%\n', sum(responsive(:,2)) / size(unitData, 1) * 100);
 fprintf('EHW Responsive : %.2f %%\n', sum(responsive(:,3)) / size(unitData, 1) * 100);
 
-%% 
-
 %% Venn Diagram
 num = 0;
 target = [1,1,1];
@@ -43,6 +41,35 @@ for i = 1 : size(unitData,1)
 end
 disp(num);
 
+%% How many cells are aligned to the center
+bin_size = 80;
+bin_center_size = 2; % 2 bins around the onset of the event (= 50ms * 2 = 100ms around the event = 200ms)
+
+data = first_LICK_zscores(logical(responsive(:,1)), :);
+[~, peak_index] = max(data, [], 2);
+[first_LICK_type_count, ~, first_LICK_type] = histcounts(peak_index, [1, bin_size/2-bin_center_size+1, bin_size/2+bin_center_size+1, 80]);
+first_LICK_type_range = cumsum(first_LICK_type_count);
+array2append = zeros(size(responsive,1),1);
+array2append(logical(responsive(:,1))) = first_LICK_type;
+output = [output, table(array2append, 'VariableNames', "first_LICK_type")];
+
+data = valid_IROF_A_zscores(logical(responsive(:,2)), :);
+[~, peak_index] = max(data, [], 2);
+[valid_IROF_A_type_count, ~, valid_IROF_A_type] = histcounts(peak_index, [1, bin_size/2-bin_center_size+1, bin_size/2+bin_center_size+1, 80]);
+valid_IROF_A_type_range = cumsum(valid_IROF_A_type_count);
+array2append = zeros(size(responsive,1),1);
+array2append(logical(responsive(:,2))) = valid_IROF_A_type;
+output = [output, table(array2append, 'VariableNames', "valid_IROF_A_type")];
+
+data = valid_IROF_E_zscores(logical(responsive(:,3)), :);
+[~, peak_index] = max(data, [], 2);
+[valid_IROF_E_type_count, ~, valid_IROF_E_type] = histcounts(peak_index, [1, bin_size/2-bin_center_size+1, bin_size/2+bin_center_size+1, 80]);
+valid_IROF_E_type_range = cumsum(valid_IROF_E_type_range);
+array2append = zeros(size(responsive,1),1);
+array2append(logical(responsive(:,3))) = valid_IROF_E_type;
+output = [output, table(array2append, 'VariableNames', "valid_IROF_E_type")];
+
+
 %% Draw Peak Sorted PETH - Lick vs AHW vs EHW
 figureSize = [89, 248, 288, 689];
 figure('Name', 'SortedPETH_Lick', 'Position', figureSize);
@@ -51,9 +78,9 @@ ax_hist1 = subplot(4,1,4);
 drawPeakSortedPETH(first_LICK_zscores(logical(responsive(:,1)), :), [-2000, 2000], 50, ax_hm1, ax_hist1, 'Name', 'First Lick');
 ax_hm1.Clipping = 'off';
 hold(ax_hm1, 'on');
-fill(ax_hm1, [82, 82, 85, 85], [1, 139, 139, 1], xkcd.red, 'LineStyle', 'None');
-fill(ax_hm1, [82, 82, 85, 85], [139, 163, 163, 139], xkcd.goldenrod, 'LineStyle', 'None');
-fill(ax_hm1, [82, 82, 85, 85], [163, 229, 229, 163], xkcd.blue, 'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [1, first_LICK_type_range(1), first_LICK_type_range(1), 1], xkcd.red, 'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [first_LICK_type_range(1), first_LICK_type_range(2), first_LICK_type_range(2), first_LICK_type_range(1)], xkcd.goldenrod, 'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [first_LICK_type_range(2), first_LICK_type_range(3), first_LICK_type_range(3), first_LICK_type_range(2)], xkcd.blue, 'LineStyle', 'None');
 
 figure('Name', 'SortedPETH_AHW', 'Position', figureSize);
 ax_hm1 = subplot(4,1,1:3);
@@ -61,9 +88,9 @@ ax_hist1 = subplot(4,1,4);
 drawPeakSortedPETH(valid_IROF_A_zscores(logical(responsive(:,2)), :), [-2000, 2000], 50, ax_hm1, ax_hist1, 'Name', 'AHW');
 ax_hm1.Clipping = 'off';
 hold(ax_hm1, 'on');
-fill(ax_hm1, [82, 82, 85, 85], [1, 73, 73, 1], xkcd.red, 'LineStyle', 'None');
-fill(ax_hm1, [82, 82, 85, 85], [73, 104, 104, 73], xkcd.goldenrod, 'LineStyle', 'None');
-fill(ax_hm1, [82, 82, 85, 85], [104, 159, 159, 104], xkcd.blue, 'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [1, valid_IROF_A_type_range(1), valid_IROF_A_type_range(1), 1], xkcd.red, 'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [valid_IROF_A_type_range(1), valid_IROF_A_type_range(2), valid_IROF_A_type_range(2), valid_IROF_A_type_range(1)], xkcd.goldenrod, 'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [valid_IROF_A_type_range(2), valid_IROF_A_type_range(3), valid_IROF_A_type_range(3), valid_IROF_A_type_range(2)], xkcd.blue, 'LineStyle', 'None');
 
 figure('Name', 'SortedPETH_EHW', 'Position', figureSize);
 ax_hm1 = subplot(4,1,1:3);
@@ -71,40 +98,30 @@ ax_hist1 = subplot(4,1,4);
 drawPeakSortedPETH(valid_IROF_E_zscores(logical(responsive(:,3)), :), [-2000, 2000], 50, ax_hm1, ax_hist1, 'Name', 'EHW');
 ax_hm1.Clipping = 'off';
 hold(ax_hm1, 'on');
-fill(ax_hm1, [82, 82, 85, 85], [1, 36, 36, 1], xkcd.red,'LineStyle', 'None');
-fill(ax_hm1, [82, 82, 85, 85], [36, 160, 160, 36], xkcd.goldenrod, 'LineStyle', 'None');
-fill(ax_hm1, [82, 82, 85, 85], [160, 215, 215, 160], xkcd.blue, 'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [1, valid_IROF_E_type_range(1), valid_IROF_E_type_range(1), 1], xkcd.red,'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [valid_IROF_E_type_range(1), valid_IROF_E_type_range(2), valid_IROF_E_type_range(2), valid_IROF_E_type_range(1)], xkcd.goldenrod, 'LineStyle', 'None');
+fill(ax_hm1, [82, 82, 85, 85], [valid_IROF_E_type_range(2), valid_IROF_E_type_range(3), valid_IROF_E_type_range(3), valid_IROF_E_type_range(2)], xkcd.blue, 'LineStyle', 'None');
 
-%% How many cells are aligned to the center
-bin_size = 80;
-bin_center_size = 2;
+%% Load Feature Importance Data
+load('Output.mat');
+r_ = [];
+for i = 1 : 40
+    r_ = [r_; mean(result{i}.importance_unit_score,1)'];
+end
+output = [output, table(r_, 'VariableNames', "FI")];
 
-data = first_LICK_zscores(logical(responsive(:,1)), :);
-[~, peak_index] = max(data, [], 2);
-histcounts(peak_index, [1, bin_size/2-bin_center_size+1, bin_size/2+bin_center_size+1, 80])
+%% FI
+mean(output.FI(output.first_LICK_type == 1))
+mean(output.FI(output.first_LICK_type == 2))
+mean(output.FI(output.first_LICK_type == 3))
 
-data = valid_IROF_A_zscores(logical(responsive(:,2)), :);
-[~, peak_index] = max(data, [], 2);
-histcounts(peak_index, [1, bin_size/2-bin_center_size+1, bin_size/2+bin_center_size+1, 80])
+mean(output.FI(output.valid_IROF_A_type == 1))
+mean(output.FI(output.valid_IROF_A_type == 2))
+mean(output.FI(output.valid_IROF_A_type == 3))
 
-data = valid_IROF_E_zscores(logical(responsive(:,3)), :);
-[~, peak_index] = max(data, [], 2);
-histcounts(peak_index, [1, bin_size/2-bin_center_size+1, bin_size/2+bin_center_size+1, 80])
-
-% two middle bins are selected as 'Peak near event units'
-
-
-
-
-
-
-
-
-
-
-
-
-
+mean(output.FI(output.valid_IROF_E_type == 1))
+mean(output.FI(output.valid_IROF_E_type == 2))
+mean(output.FI(output.valid_IROF_E_type == 3))
 
 
 
