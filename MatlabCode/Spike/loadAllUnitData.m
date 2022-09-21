64c669496cef58585b2df4611df7462c47aca3d4
Knowblesse <knowblesse@gmail.com>
2022-09-21 15:10:48 +0900

    z score method changed

--- MatlabCode/Spike/loadAllUnitData.m
+++ MatlabCode/Spike/loadAllUnitData.m
@@ -44,24 +44,25 @@ for f = 1 : numel(workingfile)
         clearvars SU;
 
         %Zscore
-        % See Devlog on 2022SEP16
-        baseline_mean = Neurons{n}.mean_baseline;
-        baseline_std = Neurons{n}.std_baseline;
-
         Zscore = struct();
         
         Zscore.valid_IRON = Neurons{n}.zscore.valid_IRON;
         Zscore.valid_IROF = Neurons{n}.zscore.valid_IROF;
-
+        
+        % use equal number of trials when calculating mean and std for baseline.
+        % see 
         Zscore.first_LICK_A = ...
-            ( mean(Neurons{n}.binned_spike.first_LICK(behaviorResult == 'A',:), 1) - baseline_mean ) ./ baseline_std;
+            ( mean(Neurons{n}.binned_spike.first_LICK(behaviorResult == 'A',:), 1) - mean(mean(Neurons{n}.binned_spike.TRON(behaviorResult == 'A',:), 1))) ./ ...
+            std( mean(Neurons{n}.binned_spike.TRON(behaviorResult == 'A',:), 1) );
         Zscore.first_LICK_E = ...
-            ( mean(Neurons{n}.binned_spike.first_LICK(behaviorResult == 'E',:), 1) - baseline_mean ) ./ baseline_std;
-
+            ( mean(Neurons{n}.binned_spike.first_LICK(behaviorResult == 'E',:), 1) - mean(mean(Neurons{n}.binned_spike.TRON(behaviorResult == 'E',:), 1))) ./ ...
+            std( mean(Neurons{n}.binned_spike.TRON(behaviorResult == 'E',:), 1) );
         Zscore.valid_IROF_A = ...
-            ( mean(Neurons{n}.binned_spike.valid_IROF(behaviorResult == 'A',:), 1) - baseline_mean ) ./ baseline_std;
+            ( mean(Neurons{n}.binned_spike.valid_IROF(behaviorResult == 'A',:), 1) - mean(mean(Neurons{n}.binned_spike.TRON(behaviorResult == 'A',:), 1))) ./ ...
+            std( mean(Neurons{n}.binned_spike.TRON(behaviorResult == 'A',:), 1) );
         Zscore.valid_IROF_E = ...
-            ( mean(Neurons{n}.binned_spike.valid_IROF(behaviorResult == 'E',:), 1) - baseline_mean ) ./ baseline_std;
+            ( mean(Neurons{n}.binned_spike.valid_IROF(behaviorResult == 'E',:), 1) - mean(mean(Neurons{n}.binned_spike.TRON(behaviorResult == 'E',:), 1))) ./ ...
+            std( mean(Neurons{n}.binned_spike.TRON(behaviorResult == 'E',:), 1) );
 
         % save
         output(i_output,:) = {subject, string(TANK_name), string(brainArea), n, Neurons(n), spikes, ParsedData, behaviorResult, {Zscore}};
