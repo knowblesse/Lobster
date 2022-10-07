62d687cde69d45189e853b5417f247461d5c0c07
Knowblesse <knowblesse@gmail.com>
2022-10-07 17:34:41 +0900

    hyperparameter optimized

--- PythonCode/EventClassification/EventClassifier_AE.py
+++ PythonCode/EventClassification/EventClassifier_AE.py
@@ -1,27 +1,26 @@
 """
 EventClassifier_AE
-@ 2020 Knowblesse
+@ 2022 Knowblesse
 Using the preprocessed Neural Ensemble dataset with behavior labels, build and test the SVM.
 Along with the accuracy, feature importance is calculated.
 Divide all dataset into Head Entry and Head Withdrawal, and generate two svm for A/E classification.
 - Description
     - .mat dataset must have two variable, X and y. (mind the case of the variable name)
     - using the sklearn SVC class, build and test the SVM
-    - for the evalutation, Leave One Out method is used
+    - for the evaluation, Leave One Out method is used
 """
-
 import numpy as np
 from numpy.random import default_rng
 from pathlib import Path
 import sklearn
-from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
 from sklearn.model_selection import LeaveOneOut
-from sklearn.metrics import accuracy_score, balanced_accuracy_score
+from sklearn.metrics import balanced_accuracy_score
 from scipy.io import loadmat, savemat
 from tqdm import tqdm
-import requests
 import re
+import platform
+
 # Check package version
 if (sklearn.__version__ < '0.23.2'):
     raise Exception("scikit-learn package version must be at least 0.23.2")
@@ -33,12 +32,12 @@ def EventClassifier(matFilePath, numBin, unit_list):
     # Define Classification function
     def runTest(X,y):
         # Leave One Out, and collect all predict result
-        y_pred = np.zeros((len(y),), dtype='uint8')
+        y_pred = np.zeros((len(y),), dtype=int)
         loo = LeaveOneOut()
         for train_index, test_index in loo.split(X):
             X_train, X_test = X[train_index], X[test_index]
             y_train = y[train_index]
-            clf = SVC(C=2, kernel='linear')
+            clf = SVC(C=6, kernel='sigmoid', gamma='scale', coef0=0.6)
             clf.fit(X_train, y_train)
             y_pred[test_index] = clf.predict(X_test)
         return y_pred
@@ -52,8 +51,7 @@ def EventClassifier(matFilePath, numBin, unit_list):
     X = np.clip(X, -5, 5)
 
     # Divide HE and HW dataset
-
-    X_HE = X[np.any([(y == 1), (y == 2)], 0), :]
+    X_HE = X[np.any([(y == 1), (y == 2)], 0), :] # remember that the class label starts from 1
     X_HW = X[np.any([(y == 3), (y == 4)], 0), :]
     y_HE = y[np.any([(y == 1), (y == 2)], 0)]
     y_HW = y[np.any([(y == 3), (y == 4)], 0)]
@@ -74,7 +72,7 @@ def EventClassifier(matFilePath, numBin, unit_list):
         y_pred_real = runTest(X, y_real)
 
         # Run which unit is important
-        numRepeat = 10
+        numRepeat = 100
         numUnit = int(X.shape[1] / numBin)
 
         baseScore = balanced_accuracy_score(y_real, y_pred_real)
@@ -114,7 +112,7 @@ def EventClassifier(matFilePath, numBin, unit_list):
         'importance_score_HW': importance_score_AE[1],
         }
 
-def Batch_EventClassifier(baseFolderPath):
+def Batch_EventClassifier(baseFolderPath, unit_list):
     # run through all dataset and generate result summary
     result = []
     tankNames = []
@@ -122,10 +120,6 @@ def Batch_EventClassifier(baseFolderPath):
     balanced_accuracy = np.empty((0,2))
 
     pbar = tqdm(sorted([p for p in baseFolderPath.glob('#*')]))
-
-    # unit list of AHW Class 1 unit
-    data = loadmat('/home/ainav/Data/EventClassificationData_4C/AHW_C1_unit_list.mat')
-    unit_list = data.get('AHW_C1_unit_list')
 
     for i, dataPath in enumerate(pbar):
         pbar.set_postfix({'path':dataPath})
@@ -141,6 +135,18 @@ def Batch_EventClassifier(baseFolderPath):
             result.append(data_)
 
     return {'tankNames' : tankNames, 'result' : result, 'sessionNames': sessionNames}
-    
-output = Batch_EventClassifier(Path(r'/home/ainav/Data/EventClassificationData_4C'))
-savemat(r'/home/ainav/Data/EventClassificationResult_4C/Output_AE_AHW_C1.mat', output)
+
+if platform.system() == 'Windows':
+    # unit list of AHW Class 1 unit
+    data = loadmat('C:\VCF\Lobster\MatlabCode\ML\AHW_C1_unit_list.mat')
+    unit_list = data.get('AHW_C1_unit_list')
+
+    output = Batch_EventClassifier(Path(r'D:\Data\Lobster\EventClassificationData_4C'), unit_list)
+    savemat(r'D:\Data\Lobster\EventClassificationResult_4C\Output_AE_AHW_C1.mat', output)
+else:
+    # unit list of AHW Class 1 unit
+    data = loadmat('/home/ainav/Data/EventClassificationData_4C/AHW_C1_unit_list.mat')
+    unit_list = data.get('AHW_C1_unit_list')
+
+    output = Batch_EventClassifier(Path(r'/home/ainav/Data/EventClassificationData_4C'), unit_list)
+    savemat(r'/home/ainav/Data/EventClassificationResult_4C/Output_AE_AHW_C1.mat', output)
