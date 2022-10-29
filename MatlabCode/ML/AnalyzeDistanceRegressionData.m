a56fd46bb80f8f1f69d193442a84128b748462bc
Knowblesse <knowblesse@gmail.com>
2022-10-29 22:45:55 +0900

    Distance Regression analysis WIP

--- MatlabCode/ML/AnalyzeDistanceRegressionData.m
+++ MatlabCode/ML/AnalyzeDistanceRegressionData.m
@@ -3,31 +3,28 @@
 basePath = 'D:\Data\Lobster\DistanceRegressionResult';
 
 filelist = dir(basePath);
-sessionPaths = regexp({filelist.name},'^#\S*.csv','match');
+sessionPaths = regexp({filelist.name},'^#\S*.mat','match');
 sessionPaths = sessionPaths(~cellfun('isempty',sessionPaths));
 
 load("Apparatus.mat");
 
 %% Load Data by session
 data = cell(1,40);
-locError = cell(1,40);
+FI = cell(1,40);
 for session = 1 : 40
     TANK_name = cell2mat(sessionPaths{session});
     TANK_location = char(strcat(basePath, filesep, TANK_name));
-
-    otherTank = regexp(TANK_name, '(?<f1>.*?)_distance_.*', 'names');
-
-    xyPosition = readmatrix(fullfile('D:\Data\Lobster\LocationRegressionResult', strcat(otherTank.f1, 'result.csv')));
-
-    data{session} = [xyPosition(:,1:2), readmatrix(TANK_location)];
-    locError{session} = abs(data{session}(:,3) - data{session}(:,5));
+    load(TANK_location); % PFITestResult, WholeTestResult(row, col, true d , shuffled d, pred d)
+    
+    data{session} = WholeTestResult;
+    FI{session} = PFITestResult;
 end
 
 %% Compare Error btw shuffled and predicted
 result1 = table(zeros(40,1), zeros(40,1), 'VariableNames',["Shuffled", "Predicted"]);
 for session = 1 : 40
-    result1.Shuffled(session) = mean(abs(data{session}(:,1) - data{session}(:,2)));
-    result1.Predicted(session) = mean(abs(data{session}(:,1) - data{session}(:,3)));
+    result1.Shuffled(session) = mean(abs(data{session}(:,3) - data{session}(:,4)));
+    result1.Predicted(session) = mean(abs(data{session}(:,3) - data{session}(:,5)));
 end
 
 %% Compare Error btw Nesting zone and Foraging zone
