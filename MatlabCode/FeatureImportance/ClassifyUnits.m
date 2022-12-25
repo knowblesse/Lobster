ad9b821590d799b09b3e33d5693a17d5cbe80f8e
Knowblesse <knowblesse@gmail.com>
2022-12-25 19:09:26 +0900

    Rearranged FI codes

---
+++ MatlabCode/FeatureImportance/ClassifyUnits.m
@@ -0,0 +1,39 @@
+%% ClassifyUnits
+% Classify units into groups
+
+%% Load All Unit Data
+dataset = load('..\AllUnitData.mat');
+Unit = dataset.output;
+clearvars -except Unit;
+
+%% Extract zscores
+zscoreMatrixHE = zeros(632, 80);
+zscoreMatrixHW = zeros(632, 80);
+zscoreMatrixHW_A = zeros(632, 80);
+zscoreMatrixHW_E = zeros(632, 80);
+
+for cell = 1 : 632
+    zscoreMatrixHE(cell,:) = Unit.Zscore{cell}.first_LICK;
+    zscoreMatrixHW(cell,:) = Unit.Zscore{cell}.valid_IROF;
+    zscoreMatrixHW_A(cell,:) = Unit.Zscore{cell}.valid_IROF_A;
+    zscoreMatrixHW_E(cell,:) = Unit.Zscore{cell}.valid_IROF_E;
+end
+
+%% Sort units       
+
+[groupingResult_HE, ~] = groupUnits(zscoreMatrixHE, 'numCluster', 8, 'cutoffLimit', 50, 'showGraph', false);
+[groupingResult_HW, numGroup] = groupUnits(zscoreMatrixHW, 'numCluster', 8, 'cutoffLimit', 50, 'showGraph', false);
+[groupingResult_HW_A, numGroup] = groupUnits(zscoreMatrixHW_A, 'numCluster', 8, 'cutoffLimit', 50, 'showGraph', false);
+[groupingResult_HW_E, numGroup] = groupUnits(zscoreMatrixHW_E, 'numCluster', 8, 'cutoffLimit', 50, 'showGraph', false);
+
+
+%% Add to the Unit Data
+
+groupData = table(...
+    groupingResult_HE,...
+    groupingResult_HW,...
+    groupingResult_HW_A,...
+    groupingResult_HW_E,...
+    'VariableNames',["Group_HE", "Group_HW", "Group_HW_A", "Group_HW_E"]);
+
+Unit = [Unit, groupData];
--- MatlabCode/FeatureImportance/ConcatenateUnitData.m
+++ MatlabCode/FeatureImportance/ConcatenateUnitData.m
@@ -1,9 +1,9 @@
 %% ConcatenateUnitData
 % To create "AllUnitData_PETHincl.mat", 
 % run partially run ./Spike/SortedPETH_Scripts.m
-load('AllUnitData_PETHincl.mat');
-Unit = output;
-clearvars output
+ClassifyUnits;
+
+clearvars -except Unit
 
 %% Feature Importance - Fine Distance Regressor
 resultPath = 'D:\Data\Lobster\FineDistanceResult';
@@ -34,49 +34,6 @@ for session = 1 : 40
 end
 
 Unit = [Unit, table(FI_Event, 'VariableNames', {'FI_Distance'})];
-
-
-%% Two Event Classifier Comparison
-% Why FI is different?
-result1Path = 'D:\Data\Lobster\EventClassificationResult_4C\Output_AE_RFE_max_FI.mat';
-result2Path = 'D:\Data\Lobster\CV5_HEC_Result.mat';
-
-result1 = load(result1Path);
-result2 = load(result2Path);
-
-score1 = zeros(40,3);
-score2 = zeros(40,2);
-
-FI1 = [];
-FI2 = [];
-
-for session = 1 : 40
-    score1(session, :) = result1.result{session}.balanced_accuracy_HW;
-    score2(session, :) = result2.result{session}.balanced_accuracy_HWAE;
-
-    numCell = numel(result1.result{session}.unitRank_HE);
-    fi_ = zeros(numCell,1);
-    fi_(result1.result{session}.importanceUnit_HW+1) = result1.result{session}.importanceScore_HW;
-    FI1 = [FI1; fi_];
-    
-    FI2 = [FI2; permutation_feature_importance(result2.result{session}.WholeTestResult_HWAE, result2.result{session}.PFITestResult_HWAE)];
-end
-
-
-corr(FI1(Unit.Session ~= "#21JAN5-210803-182450_IL"), FI2(Unit.Session ~= "#21JAN5-210803-182450_IL"))
-corr(FI1(FI1 > 0 & Unit.Session ~= "#21JAN5-210803-182450_IL"), FI2(FI1 > 0 & Unit.Session ~= "#21JAN5-210803-182450_IL"))
-
-
-%% Put Event Classifier Score into Unit table
-Unit = [Unit, table(zeros(632,3), 'VariableNames', "EC_Score")];
-result1 = load('D:\Data\Lobster\EventClassificationResult_4C\Output_AE_RFE_max_FI.mat');
-result1.sessionNames = string(result1.sessionNames);
-for session = 1 : 40
-    Unit.EC_Score(Unit.Session == result1.sessionNames(session), :) = ...
-        repmat(result1.result{session}.balanced_accuracy_HW, sum(Unit.Session == result1.sessionNames(session)), 1);
-end
-
-
 
 %% Feature Importance - Event Classifier
 Unit = [Unit, table(zeros(size(Unit,1),1), zeros(size(Unit,1),1), zeros(size(Unit,1),1), 'VariableNames', {'FI_Event_Importance', 'FI_Event_Score', 'FI_Event_Score_Relative'})];
