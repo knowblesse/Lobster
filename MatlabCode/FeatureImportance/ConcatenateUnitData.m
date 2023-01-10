e4e4d673402bef2a9f154729a77060ceaa54b5a7
Knowblesse <knowblesse@gmail.com>
2023-01-10 16:52:59 +0900

    FI scripts changed

--- MatlabCode/FeatureImportance/ConcatenateUnitData.m
+++ MatlabCode/FeatureImportance/ConcatenateUnitData.m
@@ -2,6 +2,7 @@
 ClassifyUnits;
 
 clearvars -except Unit
+px2cm = 0.169;
 
 %% Feature Importance - Fine Distance Regressor
 resultPath = 'D:\Data\Lobster\FineDistanceResult';
@@ -44,48 +45,54 @@ end
 Unit = [Unit, table(FI_Distance_Ratio, FI_Distance_Difference, FI_Distance_Relative, 'VariableNames', {'FI_Distance_Ratio', 'FI_Distance_Difference', 'FI_Distance_Relative'})];
 
 %% Feature Importance - Event Classifier
-resultPath = 'D:\Data\Lobster\BNB_Result_unitshffle.mat';
+resultPath = 'D:\Data\Lobster\BNB_Result_fullshuffle.mat';
 
 load(resultPath);
 
 sessionNames = string(sessionNames);
 
-
-FI_Event_Difference = [];
-EC_Score = [];
+EC_Score = []; % score of the session
+FI_EC_ACC = []; % FI using accuracy difference
+FI_EC_CE = []; % FI using cross entropy difference
+FI_EC_FP = []; % FI using feature probabilities
 
 for session = 1 : 40
-    
-    FI_Event_Difference = [FI_Event_Difference; result{session}.PFICrossEntropy_HWAE];
-%     numBin = 40;
-%     numCell = numel(result{session}.PFICrossEntropy_HEAE);
-%     mean(...
-%         squeeze(...
-%             sum(...
-%                 reshape(...
-%                     permute(result{session}.feature_prob_HWAE, [3,2,1]),...
-%                     numBin, numCell, 2, 5)...
-%                 ,1)... % sum of all log(Prob) from all units. => joint Prob.
-%             )...
-%         ,3)
+    fprintf('%s\n', sessionNames{session});
+    numBin = 40;
+    numCell = numel(result{session}.PFICrossEntropy_HEAE);
 
     EC_Score = [EC_Score;...
         repmat([...
             result{session}.balanced_accuracy_HEHW,...
             result{session}.balanced_accuracy_HEAE,...
             result{session}.balanced_accuracy_HWAE],...
-            numel(result{session}.PFICrossEntropy_HWAE), 1)...
+            numCell, 1)...
         ];
+    
+    FI_EC_ACC = [FI_EC_ACC;...
+        permutation_feature_importance(result{session}.WholeTestResult_HWAE >= 0.5, result{session}.PFITestResult_HWAE >= 0.5, 'method','difference')];
+
+    FI_EC_CE = [FI_EC_CE;...
+        result{session}.PFICrossEntropy_HWAE];
+    
+    t = reshape(...
+                    permute(result{session}.feature_prob_HWAE, [3,2,1]),... % 
+                    numBin, numCell, 2, 5);
+
+    logFeatureProb = mean(...
+        squeeze(...
+            sum(...
+                t(1:15,:,:,:)...
+                ,1)... % sum of all log(Prob) from all units. => joint Prob.
+            )...
+        ,3);
+    FI_EC_FP = [FI_EC_FP; oddRatio(logFeatureProb(:,1), logFeatureProb(:,2))];
 end
 
-Unit = [Unit, table(FI_Event_Difference, EC_Score, 'VariableNames', {'FI_Event_Difference', 'EC_Score'})];
-Unit.FI_Event_Difference(Unit.FI_Event_Difference < 0) = 0;
+Unit = [Unit, table(EC_Score, FI_EC_ACC, FI_EC_CE, FI_EC_FP, 'VariableNames', {'EC_Score', 'FI_EC_ACC','FI_EC_CE','FI_EC_FP'})];
+clearvars -except Unit
 
 %% 
-
 [h, p] = ttest2(...
-    Unit.FI_Event_Difference((Unit.Group_HW_E ~= 1) & (Unit.EC_Score(:,6) > 0.6)),...
-    Unit.FI_Event_Difference((Unit.Group_HW_E == 1) & (Unit.EC_Score(:,6) > 0.6)))
-
-
-
+    abs(Unit.FI_EC_FP((Unit.Group_HW_A == 0) & (Unit.EC_Score(:,6) > 0.6))),...
+    abs(Unit.FI_EC_FP((Unit.Group_HW_A == 3) & (Unit.EC_Score(:,6) > 0.6))))
