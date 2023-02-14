b0a0b1b1db2721e6c8e65e7fdee064af17a2fcc1
Knowblesse <knowblesse@gmail.com>
2023-02-14 18:43:09 +0900

    Distracted Data analysis script WIP

---
+++ MatlabCode/PatternSwitch/DistractedDataScript.m
@@ -0,0 +1,32 @@
+%% DistractedDataScript
+% Script for analyzing hand-labeled distracted data
+% Warning : the bool_distracted only contains values inside the nest zone.
+
+px2cm = 0.169;
+truncatedTimes_s = 10;
+neural_data_rate = 20;
+
+numTank = 24;
+output_table = table(strings(numTank, 1), zeros(numTank, 1), zeros(numTank, 1), zeros(numTank, 2),...
+    'VariableNames', {'Tank', 'Error_Distracted', 'Error_Engaged', 'NumData'});
+
+%% 
+for i = 1 : numTank
+    tank_name = tank_list(i);
+    load("D:\Data\Lobster\FineDistanceDataset\" + tank_name + "\" + tank_name + "_frameInfo.mat");
+    load("D:\Data\Lobster\FineDistanceResult_syncFixed\" + tank_name + "result_distance.mat");
+    distracted = readmatrix("D:\Data\Lobster\Lobster_Recording-200319-161008\Data\" + tank_name + "\bool_distracted.csv");
+
+    midPointTimes = truncatedTimes_s + (1/neural_data_rate)*(0:size(WholeTestResult,1)-1) + 0.5 * (1/neural_data_rate);
+
+    % Construct Frame Number correction
+    neuralData_isDistracted = logical(distracted(round(interp1(frameTime, frameNumber, midPointTimes))));
+    isNesting = WholeTestResult(:,2) < 225;
+
+    % Compare Error btw shuffled and predicted
+    output_table.Tank(i) = tank_name;
+    output_table.Error_Distracted(i) = mean(abs(WholeTestResult(neuralData_isDistracted & isNesting, 3) - WholeTestResult(neuralData_isDistracted & isNesting, 5)) * px2cm);
+    output_table.Error_Engaged(i) = mean(abs(WholeTestResult((~neuralData_isDistracted) & isNesting, 3) - WholeTestResult((~neuralData_isDistracted) & isNesting, 5)) * px2cm);
+    output_table.NumData(i,:) = [sum(neuralData_isDistracted & isNesting), sum((~neuralData_isDistracted) & isNesting)];
+    
+end
