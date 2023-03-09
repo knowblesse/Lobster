8b926d1fe5a54bfd20603730de76f54ea571183f
Knowblesse <knowblesse@gmail.com>
2023-03-09 15:23:38 +0900

    FI draw unsupervised cells

--- MatlabCode/FeatureImportance/groupUnits.m
+++ MatlabCode/FeatureImportance/groupUnits.m
@@ -24,16 +24,34 @@ for clt = 1 : options.numCluster
 end
 
 if options.showGraph
+    if numGroup == 2
+        colors_bw = [0, 0, 0; 0.5, 0.5, 0.5];
+    elseif numGroup == 3
+        colors_bw = [0, 0, 0; 0.4, 0.4, 0.4; 0.8, 0.8, 0.8];
+    end
     fig = figure();
     axes();
     hold on;
     lines = [];
     legends = {};
     for group = 1 : numGroup
-        [~, obj_line, ~] = shadeplot(zscoreMatrix(groupingResult == group, :), 'SD', 'sem', 'LineWidth', sum(groupingResult == group)/100);
+        [~, obj_line, ~] = shadeplot(...
+            zscoreMatrix(groupingResult == group, :),...
+            'SD', 'sem',... %'LineWidth', sum(groupingResult == group)/100,...
+            'LineWidth', 1.3,...
+            'FaceAlpha', 0.3,...
+            'Color', colors_bw(group,:));
         lines = [lines, obj_line];
-        legends = [legends, strcat("Group : " , num2str(group), " #", num2str(sum(groupingResult == group)))];
+        legends = [legends, strcat("Group " , num2str(group), " #", num2str(sum(groupingResult == group)))];
     end
-    legend(lines, legends);
+    line(xlim, [0,0], 'LineStyle', ':', 'Color', [0.3, 0.3, 0.3]);
+    ylabel('Z score');
+    xlabel('Time (sec)');
+    xticks(0:20:80);
+    xticklabels(-1:0.5:1);
+    legend(lines, legends, 'FontSize', 6.6);
+    set(gca, 'FontName', 'Noto Sans');
+    pos = get(gcf, 'Position');
+    set(gcf, 'Position', [pos(1), pos(2), 357, 236]);
 end
 end
