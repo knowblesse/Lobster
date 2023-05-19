787003a753a71a5de91ff907b50f3a08e2ee09ce
Knowblesse <knowblesse@gmail.com>
2023-05-19 22:17:13 +0900

    draw HE1-HW1 and HE2-HW2 graph

--- MatlabCode/FeatureImportance/ClassifyUnits.m
+++ MatlabCode/FeatureImportance/ClassifyUnits.m
@@ -39,3 +39,40 @@ Unit = [Unit, groupData];
 
 %% Generate Table
 drawConfusionMatrix(groupingResult_HE, groupingResult_HW, 'ShowSum', true, 'ColName', "HW Group", "RowName", "HE Group", "Color", [0, 0, 0]);
+
+%% Draw HE1-HW1 and HE2-HW2 graph
+
+groupingResult = zeros(size(Unit,1),1);
+groupingResult(groupData.Group_HE == 1 & groupData.Group_HW == 1) = 1;
+groupingResult(groupData.Group_HE == 2 & groupData.Group_HW == 2) = 2;
+
+colors_bw = [0, 0, 0; 0.5, 0.5, 0.5];
+for matrix = {zscoreMatrixHE, zscoreMatrixHW}
+    zscoreMatrix = matrix{1};
+
+    fig = figure();
+    axes();
+    hold on;
+    lines = [];
+    legends = {};
+    for group = 1 : 2
+        [~, obj_line, ~] = shadeplot(...
+            zscoreMatrix(groupingResult == group, :),...
+            'SD', 'sem',... %'LineWidth', sum(groupingResult == group)/100,...
+            'LineWidth', 1.3,...
+            'FaceAlpha', 0.3,...
+            'Color', colors_bw(group,:));
+        lines = [lines, obj_line];
+    end
+    line(xlim, [0,0], 'LineStyle', ':', 'Color', [0.3, 0.3, 0.3]);
+    ylabel('Z score');
+    xlabel('Time (sec)');
+    xticks(0:20:80);
+    xticklabels(-2:2);
+    legend(lines, {'HE1-HW1', 'HE2-HW2'}, 'FontSize', 6.6);
+    set(gca, 'FontName', 'Noto Sans');
+    pos = get(gcf, 'Position');
+    set(gcf, 'Position', [pos(1), pos(2), 288, 236]);
+end
+
+
